36e395b0c35794566d6f2b2a98cc08619b29bbed
Kars <k@kars.bio>
2024-10-08 09:27:15 +0100

    feat: Custom key generation

---
+++ src/utils/key.ts
@@ -0,0 +1,15 @@
+import * as crypto from "crypto-js";
+
+const privatekey = process.env.PRIVATE_KEY || "privatekey123";
+
+export default function getKey(): string {
+    const date = new Date();
+    const datestr = date.toISOString().split("T")[0];
+
+    const hours = date.getUTCHours();
+    const minutes = Math.floor(date.getMinutes() / 3);
+    const time = `${hours}:${minutes.toString().padStart(2, "0")}`;
+    const combined = `${privatekey}${datestr}${time}`;
+    const hash = CryptoJS.SHA256(combined).toString();
+    return hash.substring(0, 16);
+}
